e7cb76bb9694e9b2b3cbbb3d882c6a56d35ae37f
Analaumg <116237480+Analaumg@users.noreply.github.com>
2023-05-25 04:16:46 +0000

    Creacion de proyeecto con nextjs

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+<h1>FLAMME ROUGE BY ALM</h1>
+
+  )
+}
